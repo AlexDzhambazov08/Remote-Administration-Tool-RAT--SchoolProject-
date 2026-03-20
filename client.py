b86b4b7ca37b55a15b0f751dc229526639e436bb
Viktor Petkov <viktor.petkov@gikn.eu>
2026-03-20 11:33:39 +0200

    Fix server prompts being entered as client input

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import socket
+import sys
 
-SERVER_IP = input("Enter server IP: ")
+SERVER_IP = sys.stdin.readline().strip()# input("Enter server IP: ")
 PORT = 5050
-code = input("Enter one-time code: ").strip()
+code = sys.stdin.readline().strip()# input("Enter one-time code: ").strip()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, PORT))
@@ -11,13 +12,13 @@ message = f"CONNECT {code}"
 client_socket.sendall(message.encode())
 
 response = client_socket.recv(1024).decode()
-print("Server response:", response)
+print("Server response:", response, flush=True)
 
 # Enter command loop
 if response == "ACCEPT":
     try:
         while True:
-            cmd = input("Enter command (use '/exit' or 'exit' to quit): ").strip()
+            cmd = sys.stdin.readline().strip()# input("Enter command (use '/exit' or 'exit' to quit): ").strip()
             
             if not cmd:
                 continue
@@ -28,7 +29,7 @@ if response == "ACCEPT":
                 
             # Wait for server reply
             reply = client_socket.recv(1024).decode()
-            print("Server reply:", reply)
+            print("Server reply:", reply, flush=True)
     
     except (ConnectionResetError, BrokenPipeError):
         print("Connection closed by server.")
